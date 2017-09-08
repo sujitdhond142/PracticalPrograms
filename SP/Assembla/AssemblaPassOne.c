f6c327ffd4a27006c82fd2a5263fb03865d64f00
Ankush Patil <aspraz2658@gmail.com>
2017-09-08 21:18:48 +0530

    started out by asprazz + sujit + waiting somebody to join

--- SP/Assembla/AssemblaPassOne.c
+++ SP/Assembla/AssemblaPassOne.c
@@ -4,8 +4,63 @@
 #include <string.h>
 #include <math.h>
 
+
+//definations here 
+#define ON 1
+#define OFF 0
+#define DEBUG 1
+
+
+
+//functions declaratins here
+
+
+
+
+
+
+
+//starting of the main program
 int main() {
+
     // assembla designing and developement by tybtech computer 
+    
+    //declaring variables 
+    char choice,continueChoice;
+
+
+    do {
+        printf("________________MENU__________________");
+        printf("1. Enter the program here");
+        printf("2. Search in POT");
+        printf("3. Search in MOT");
+        printf("4. Create Symbol Table");
+        printf("5. Create the literal Table");
+        printf("Enter your chioce");
+        scanf("%s",&choice);
+
+        switch(choice) {
+            
+
+            case '1':break;
+            case '2':break;
+            case '3':break;
+            case '4':break;
+            case '5':break;
+            default: printf("Invalid chocie");
+
+        }
+        printf("Do you want to continue ? ");
+        scanf("%s",&continueChoice);
+    
+    }while(continueChoice == 'y' || continueChoice ==  'Y');
 
     return 0;
 }
+
+//definations of functions 
+
+
+
+
+
