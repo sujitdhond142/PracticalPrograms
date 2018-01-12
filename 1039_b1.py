ba52c814b2376f81b2c6d5fc2b9276551f487299
Ankush Patil <aspraz2658@gmail.com>
2018-01-12 17:51:06 +0530

    1541039 is ready for detach

---
+++ 1039_b1.py
@@ -0,0 +1,115 @@
+'''
+    Aim   : Practice Programs
+    Name  : Ankush Patil
+    Prn   : 1541039
+    Batch : T2
+'''
+#importing libraries
+import os
+import math
+
+
+def Display_menu():
+    dec = 'y'
+    while(checkDecision(dec)):
+        
+        #clearing the terminal 
+        os.system('clear')
+
+        print "_______________MENU_________________"
+        print "1.Find square root of no : "
+        print "2.Find area of rectangle : "
+        print "3.Swap two nos : "
+        print "4.Data Structures: list,dictonaries,tuples"
+        print "5.Find No. is even or odd"
+        print "6.Largest among three no.s"
+        print "7.Check if year is leap or not"
+        print "8.Fibonacci Sequence"
+        print "9.While loop"
+        print "10.Prime no upto n nos:"
+        print "11.Perform operations on word \"govenrmentcollege\" "
+        
+
+        try:
+            choice = int(input("Enter your choice : "))
+
+        except NameError:
+            print ""
+            print "That's not a number"
+            continue
+
+        #summing it up in options dictionery
+    
+        options = { 1  : findSqrt,
+                    2  : findArea,
+                    3  : swapper,
+                    4  : useDS,
+                    5  : findEvenOdd,
+                    6  : findLargest,
+                    7  : isLeap,
+                    8  : findFibbo,
+                    9  : useWhile,
+                    10 : findPrimeUptoN,
+                    11 : operation
+        }
+
+        #checking choice is available or not
+        try:
+            options[choice]()
+        except:
+            print ""
+            print "Invalid Choice Please Enter Valid One"
+
+        print "____________________________________________"
+        print ""
+        dec = raw_input("Do you want to try again (y/n) : ")
+        print ""
+
+
+
+
+#supportive functions 
+def checkDecision(ans):
+    #converting ans to lower first
+    if ans.lower() == 'y':
+        return True 
+    else:
+        return False
+
+def findSqrt():
+    print "in"
+
+def findArea():
+    print "in"
+
+def findEvenOdd():
+    print "in"
+
+def findLargest():
+    print "in"
+
+def findFibbo():
+    print "in"
+
+def swapper():
+    print "in"
+
+def isLeap():
+    print "in"
+
+def useDS():
+    print "in"
+
+def useWhile():
+    print "in"
+
+def findPrimeUptoN():
+    print "in"
+
+def operation():
+    print "in"
+
+
+
+if __name__=="__main__":
+    Display_menu()
